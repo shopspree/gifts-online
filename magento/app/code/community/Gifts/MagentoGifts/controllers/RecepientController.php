165b4e1dd752ea5d4ea08a29d67c71ddf3f8d030
asaf <asaf.ach@gmail.com>
2012-10-26 23:34:15 +0200

    magento base recepient and buyer landing page

---
+++ magento/app/code/community/Gifts/MagentoGifts/controllers/RecepientController.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Gifts frontend controller
+ * 
+ * @author asafam
+ */
+
+class Gifts_MagentoGifts_RecepientController extends Mage_Core_Controller_Front_Action
+{
+	/**
+	 * 
+	 */
+	function indexAction() {
+		if ($this->getRequest()->isPost()){
+			$int1 = $this->getRequest()->getPost('int1');
+			$int2 = $this->getRequest()->getPost('int2');
+			$result = $int1 * $int2;
+			Mage::getSingleton('customer/session')->addSuccess("$int1 * $int2 = $result");
+		}
+		
+		$this->loadLayout();
+		
+		$this->_initLayoutMessages('customer/session');
+		
+		$this->renderLayout();
+	}
+	
+}
+
